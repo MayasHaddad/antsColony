2c199d7107ec8aa0bfd656b30e8c3adbbdcd0ddc
Mayas Haddad <mayas_91@hotmail.com>
2014-01-17 15:14:09 +0100

    added pathFinder, pathFindingPerformers still to implement + some cleaning in the sources files

--- src/antsColony/main.scala
+++ src/antsColony/main.scala
@@ -4,6 +4,7 @@
 package antsColony
 
 import scala.collection.mutable.HashMap
+import tools.fieldFiller
 
 /**
  * @author mayas
@@ -16,8 +17,8 @@ object main {
 	val fieldFilled = filler.fillField(field, 0, 0, dim)
 	
 	def main(args: Array[String]) {
-    for(i <- 0 to dim - 1){
-      for(j <- 0 to dim - 1){ 
+    for(i <- intWrapper(0) to dim - 1){
+      for(j <- intWrapper(0) to dim - 1){ 
     	  print(fieldFilled(i)(j)) 
       }
       print("\n")
